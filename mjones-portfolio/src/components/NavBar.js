ec5546f0698f1d1fdbbe560dda9ed25bc6b30ae4
Matthew Jones <matthew.a.jones0@gmail.com>
2026-04-02 16:11:44 -0700

    Updated various photos and my resume

--- mjones-portfolio/src/components/NavBar.js
+++ mjones-portfolio/src/components/NavBar.js
@@ -13,7 +13,7 @@ export default function NavBar() {
         <AppBar position="sticky" color="inherit" elevation={1}>
             <Toolbar sx={{ justifyContent: "space-between" }}>
                 <Typography variant="h6" sx={{ fontWeight: 700 }}>
-                    Matthew Jones - Electrical and Computer Engineer (767240)
+                    Matthew Jones - Electrical and Computer Engineer
                 </Typography>
                 <Box sx={{ display: "flex", gap: 1 }}>
                     <Button onClick={() => scrollTo("projects")}>Projects</Button>
